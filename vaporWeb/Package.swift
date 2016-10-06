06b3be7035446eb076cae394a87d54753c2daba9
泽熙 <zhao95@outlook.com>
2016-10-06 21:28:37 +0800

    swift 服务器   vapor 的实验

---
+++ vaporWeb/Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "vaporWeb",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
+        // MySQ 的支持
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0),
+        
+        
+        
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+        ]
+    
+)
+
---
+++ vaporWeb/Sources/App/main.swift
@@ -0,0 +1,26 @@
+import Vapor
+import VaporMySQL
+
+
+
+let drop = Droplet()
+
+drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
+    return try drop.view.make("welcome", [
+    	"message": Node.string(drop.localization[lang, "welcome", "title"])
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.get("view") { Request in
+    return try drop.view.make("view.html")
+}
+
+drop.get("fuli") { Request in
+    return try drop.view.make("fuli.html")
+}
+
+
+drop.run()
