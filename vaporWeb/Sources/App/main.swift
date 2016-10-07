5438cd42f84777708d534b6bd7315e714018bcba
泽熙 <zhao95@outlook.com>
2016-10-07 15:05:25 +0800

    实验6

--- vaporWeb/Sources/App/main.swift
+++ vaporWeb/Sources/App/main.swift
@@ -1,3 +1,6 @@
+
+// 主要swift Web主项目
+
 import Vapor
 import VaporMySQL
 
@@ -21,6 +24,20 @@ drop.get("view") { Requsest in
 drop.get("fuli") { Request in
     return try drop.view.make("fuli.html")
 }
+
+drop.get("css") { Request in
+    return try drop.view.make("03-CSS-外部样式.html")
+}
+drop.get("login") { Request in
+    return try drop.view.make("login.html")
+}
+
+drop.get("jiemian") { Request in
+    return try drop.view.make("index.html")
+}
+
+/// 研究数据库
+
 
 
 drop.run()
