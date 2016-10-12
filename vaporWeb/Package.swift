cf8c174b3ddfc8a37e6280c042bc6960719abaa5
泽熙 <zhao95@outlook.com>
2016-10-12 14:29:17 +0800

    补全

--- vaporWeb/Package.swift
+++ vaporWeb/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
         // MySQ 的支持
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0),
+//        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0),
         
        
         
